51bf0ad484317dde4f2a36550ba7c4e9984a43d0
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-26 18:04:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.${artist}
+  return playlist
+}
